9a4498189752667931f0ca5e8f918b9babff953b
scourgeCode <34277143+scourgeCode@users.noreply.github.com>
2020-10-04 20:54:04 +0200

    Test commit

---
+++ Algorithms/PlusMinus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class PlusMinus
+    {
+
+    // Complete the plusMinus function below.
+    
+    public static void plusMinus(int[] arr)
+        {
+            double positiveCount = 0;
+            double negativeCount = 0;
+            double zeroCount = 0;
+            List<double> resultList = new List<double>();
+
+            foreach (int element in arr)
+            {
+                if (element > 0)
+                    positiveCount++;
+
+                if (element < 0)
+                    negativeCount++;
+
+                if (element == 0)
+                    zeroCount++;
+            }
+
+            resultList.Add(positiveCount / arr.Length);
+            resultList.Add(negativeCount / arr.Length);
+            resultList.Add(zeroCount / arr.Length);
+
+            printList(resultList);
+
+        }
+
+        static void printList(List<double> dList)
+        {
+            foreach (double element in dList)
+                Console.WriteLine(string.Format("{0:N6}", element));
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class Exe
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            
+            PlusMinus.plusMinus(arr);
+
+            Console.ReadKey();
+        }
+
+    }
+}
